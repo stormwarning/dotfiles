b3b93710e1e2fa47043d1ce47115d661b395cdeb
Jeff <rustyangel@gmail.com>
2014-02-13 13:38:59 -0700

    add csscomb coda plug-in
    not able to submodule this yet

---
+++ ide/coda-csscomb/csscomb_for_coda_2.codaplugin/Contents/Resources/55543892-82DE-4679-9ADE-11CA109E2C68/csscomb.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+/**
+ * CSScomb
+ * @version: 2.14 (build 57f634a-1307080853)
+ * @author: Vyacheslav Oliyanchuk (miripiruni)
+ * @web: http://csscomb.com/
+ */
+require_once(getenv('TM_BUNDLE_SUPPORT').'/csscomb.php');
+$input = file_get_contents('php://stdin');
+$c = new csscomb();
+echo $c->csscomb($input);?>
