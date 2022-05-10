a8b801b84670890b399b7a93eeb32a0e338bcea7
Jeff <rustyangel@gmail.com>
2022-05-09 18:55:40 -0600

    Update Hyper settings
    - Remove `shell` config since zsh is now the default shell
    - Add new default settings

--- conf/hyper.js
+++ conf/hyper.js
@@ -22,7 +22,7 @@ module.exports = {
         fontWeightBold: 'bold',
 
         // line height as a relative unit
-        lineHeight: 1.75,
+        lineHeight: 1.5,
 
         // letter spacing as a relative unit
         letterSpacing: 0,
@@ -60,6 +60,9 @@ module.exports = {
         // custom CSS to embed in the terminal window
         termCSS: '',
 
+        // set custom startup directory (must be an absolute path)
+        workingDirectory: '',
+
         // if you're using a Linux setup which show native menus, set to false
         // default: `true` on Linux, `true` on Windows, ignored on macOS
         showHamburgerMenu: '',
@@ -92,6 +95,8 @@ module.exports = {
             lightMagenta: '#FD7CFC',
             lightCyan: '#68FDFE',
             lightWhite: '#FFFFFF',
+            limeGreen: '#32CD32',
+            lightCoral: '#F08080',
         },
 
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -101,12 +106,18 @@ module.exports = {
         // - Make sure to use a full path if the binary name doesn't work
         // - Remove `--login` in shellArgs
         //
-        // Bash on Windows
-        // - Example: `C:\\Windows\\System32\\bash.exe`
+        // Windows Subsystem for Linux (WSL) - previously Bash on Windows
+        // - Example: `C:\\Windows\\System32\\wsl.exe`
+        //
+        // Git-bash on Windows
+        // - Example: `C:\\Program Files\\Git\\bin\\bash.exe`
         //
         // PowerShell on Windows
         // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-        shell: '/usr/local/bin/zsh',
+        //
+        // Cygwin
+        // - Example: `C:\\cygwin64\\bin\\bash.exe`
+        shell: '',
 
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
@@ -115,11 +126,13 @@ module.exports = {
         // for environment variables
         env: {},
 
-        // set to `false` for no bell
+        // Supported Options:
+        //  1. 'SOUND' -> Enables the bell as a sound
+        //  2. false: turns off the bell
         bell: 'SOUND',
 
-        // if true, selected text will automatically be copied to the clipboard
-        copyOnSelect: false,
+        // An absolute file path to a sound file on the machine.
+        // bellSoundURL: '/path/to/sound/file',
 
         // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
         copyOnSelect: false,
@@ -129,10 +142,32 @@ module.exports = {
 
         // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
         // selection is present (`true` by default on Windows and disables the context menu feature)
-        // quickEdit: true,
+        quickEdit: false,
 
-        // URL to custom bell
-        // bellSoundURL: 'http://example.com/bell.mp3',
+        // choose either `'vertical'`, if you want the column mode when Option key is hold during selection (Default)
+        // or `'force'`, if you want to force selection regardless of whether the terminal is in mouse events mode
+        // (inside tmux or vim with mouse mode enabled for example).
+        macOptionSelectionMode: 'vertical',
+
+        // Whether to use the WebGL renderer. Set it to false to use canvas-based
+        // rendering (slower, but supports transparent backgrounds)
+        webGLRenderer: true,
+
+        // keypress required for weblink activation: [ctrl|alt|meta|shift]
+        // todo: does not pick up config changes automatically, need to restart terminal :/
+        webLinksActivationKey: '',
+
+        // if `false` (without backticks and without quotes), Hyper will use ligatures provided by some fonts
+        disableLigatures: true,
+
+        // set to true to disable auto updates
+        disableAutoUpdates: false,
+
+        // set to true to enable screen reading apps (like NVDA) to read the contents of the terminal
+        screenReaderMode: false,
+
+        // set to true to preserve working directory when creating splits or tabs
+        preserveCWD: true,
 
         // for advanced config flags please refer to https://hyper.is/#cfg
     },
