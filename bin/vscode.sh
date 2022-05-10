bcc40752dcb946ea6f537d7a07face351529dd16
Jeff <rustyangel@gmail.com>
2022-05-09 18:56:45 -0600

    Remove unused VS Code extensions

--- bin/vscode.sh
+++ bin/vscode.sh
@@ -10,29 +10,23 @@ ln -s ~/.dotfiles/conf/vscode-keybindings.json ~/Library/Application\ Support/Co
 
 # Install extensions.
 code --install-extension EditorConfig.EditorConfig
-code --install-extension Equinusocio.vsc-material-theme
 code --install-extension PKief.material-icon-theme
-# code --install-extension WakaTime.vscode-wakatime
 code --install-extension alefragnani.project-manager
-code --install-extension amatiasq.sort-imports
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension dhedgecock.ember-syntax
 code --install-extension eamodio.gitlens
-# code --install-extension emberjs.vscode-ember
 code --install-extension esbenp.prettier-vscode
+code --install-extension GraphQL.vscode-graphql
 code --install-extension ikappas.phpcs
 code --install-extension joelday.docthis
-code --install-extension josa.ember-related-files
-code --install-extension lifeart.vscode-ember-unstable
+code --install-extension johnsoncodehk.volar
 code --install-extension mrmlnc.vscode-scss
 code --install-extension ms-vscode.atom-keybindings
+code --install-extension ms-vscode.vscode-typescript-next
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension naumovs.color-highlight
 code --install-extension octref.vetur
-code --install-extension phanitejakomaravolu.emberes6snippets
-code --install-extension prisma.vscode-graphql
-code --install-extension shinnn.stylelint
+code --install-extension stylelint.vscode-stylelint
 code --install-extension stormwarning.json-template
 code --install-extension thebarkman.vscode-djaneiro
 
