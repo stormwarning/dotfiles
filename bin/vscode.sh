43854988275f02dbd8bfb777da9ec371a728fc68
Jeff <rustyangel@gmail.com>
2025-01-27 21:22:07 -0700

    Update VS Code settings & extensions

--- bin/vscode.sh
+++ bin/vscode.sh
@@ -9,26 +9,32 @@ ln -s ~/.dotfiles/conf/vscode-settings.json ~/Library/Application\ Support/Code/
 ln -s ~/.dotfiles/conf/vscode-keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
 # Install extensions.
-code --install-extension EditorConfig.EditorConfig
-code --install-extension PKief.material-icon-theme
 code --install-extension alefragnani.project-manager
 code --install-extension bradlc.vscode-tailwindcss
+code --install-extension csstools.postcss
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
+code --install-extension GitHub.copilot
+code --install-extension github.vscode-github-actions
+code --install-extension GitHub.vscode-pull-request-github
 code --install-extension GraphQL.vscode-graphql
 code --install-extension ikappas.phpcs
-code --install-extension joelday.docthis
-code --install-extension johnsoncodehk.volar
 code --install-extension mrmlnc.vscode-scss
-code --install-extension ms-vscode.atom-keybindings
 code --install-extension ms-vscode.vscode-typescript-next
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension naumovs.color-highlight
 code --install-extension octref.vetur
-code --install-extension stylelint.vscode-stylelint
+code --install-extension PKief.material-icon-theme
+code --install-extension sanity-io.vscode-sanity
 code --install-extension stormwarning.json-template
+code --install-extension stylelint.vscode-stylelint
+code --install-extension svelte.svelte-vscode
 code --install-extension thebarkman.vscode-djaneiro
+code --install-extension unifiedjs.vscode-mdx
+code --install-extension vitest.explorer
+code --install-extension Vue.volar
 
 # Symlink Project Manager extension settings.
 rm -r ~/Library/Application\ Support/Code/User/projects.json
